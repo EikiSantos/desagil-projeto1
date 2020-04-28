896740aa4a429886190332ee1176107ec4c6bc01
Amanda Carmo <amanda.rosa.ismart@gmail.com>
2020-04-27 21:33:15 -0300

    Tentamos cumprir o sketch

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -0,0 +1,21 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textExample = findViewById(R.id.text_example);
+        Button buttonExample = findViewById(R.id.button_example);
+
+
+    }
+}
