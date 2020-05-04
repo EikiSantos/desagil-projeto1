a28a8daa964094175328232f7942298c86353539
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-05-04 17:29:26 -0300

    Feito a mudança da cor de botão para a primeira frase do swipe

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/TranslatorActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/TranslatorActivity.java
@@ -2,6 +2,7 @@ package br.pro.hashi.ensino.desagil.projeto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
